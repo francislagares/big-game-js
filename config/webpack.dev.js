d95430451be1098e8d4d2767ef2ed544c6f0c1a1
Francis Lagares <francis.lagares@gmail.com>
2020-05-15 13:19:43 +0200

    feat: initial setup
    This commit sets up the main starter
    configuration for project development.

---
+++ config/webpack.dev.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const common = require('./webpack.common')
+const merge = require('webpack-merge')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: '#source-map',
+  output: {
+    filename: 'js/[name].[hash].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+
+  devServer: {
+    contentBase: 'dist',
+    open: true, // Tells dev-server to open the browser after server had been started
+    overlay: true, // Shows a full-screen overlay with errors or warnings
+    hot: false, // update changes without full refresh in the browser
+    inline: true,
+    stats: {
+      colors: true
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: [/.css$/],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }),
+    new HtmlWebPackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contentHash].css',
+      chunkFilename: 'css/[id].css'
+    })
+  ]
+})
