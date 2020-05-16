bf13434100b949c13f1c6eb96a516a63a2a29d95
Francis Lagares <francis.lagares@gmail.com>
2020-05-16 19:54:46 +0200

    feat: implement game functionality

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,113 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer, gamePlaying, previousRoll, winningScore;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  if(gamePlaying) {
+    // 1. Generate random number
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+    // 2. Display the result
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = `/img/dice-${dice1}.png`;
+    document.getElementById('dice-2').src = `/img/dice-${dice2}.png`;
+    
+    // 3. Update the round score IF the rolled number was NOT 1
+    if (dice1 !== 1 && dice2 !== 1) {
+      roundScore += dice1 + dice2;
+      document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
+    
+    // Use this only with one dice
+    /* if (dice === 6 && previousRoll === 6) {
+      scores[activePlayer] = 0;
+      document.getElementById(`score-${activePlayer}`).textContent = '0';
+      nextPlayer();
+    } else if (dice !== 1) {
+      roundScore += dice;
+      document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+    } else {
+      nextPlayer();    
+    } 
+
+    previousRoll = dice; */
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+  if(gamePlaying) {
+    // 1. Add current score to global score
+    scores[activePlayer] += roundScore;
+    // 2. Update the UI
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+    // 3. Let the user set up a winning score
+    let input = document.querySelector('.final-score').value;
+
+    if(input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+    // 4. Check if player won the game
+    if (scores[activePlayer] >= winningScore) {
+      document.getElementById(`name-${activePlayer}`).textContent = 'WINNER!';
+      hideDices();
+      document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+      document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }    
+  }
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+
+  hideDices();
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector(`.player-0-panel`).classList.remove('winner');
+  document.querySelector(`.player-1-panel`).classList.remove('winner');
+  document.querySelector(`.player-0-panel`).classList.remove('active');
+  document.querySelector(`.player-1-panel`).classList.remove('active');
+  document.querySelector(`.player-0-panel`).classList.add('active');
+}
+
+function hideDices() {
+  document.getElementById('dice-1').style.display = 'block';
+  document.getElementById('dice-2').style.display = 'block';
+}
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  hideDices();
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
-import '../css/styles.css'
+import '../css/styles.css';
+
+// Imports all images inside 'img' directory dinamically.
+function requireAll(r) {
+  r.keys().forEach(r);
+}
+
+requireAll(require.context('../img/', true, /\.png$/));
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
